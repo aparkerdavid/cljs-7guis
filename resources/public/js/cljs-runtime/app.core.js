167f35f87acefb493b45ed74e7d05dcf59581215
Parker Rueve <parkerrueve@Parkers-MacBook-Pro.local>
2020-12-04 00:42:41 -0500

    Update .gitignore

--- resources/public/js/cljs-runtime/app.core.js
+++
@@ -1,8 +0,0 @@
-goog.provide('app.core');
-app.core.init = (function app$core$init(){
-cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["init!"], 0));
-
-return reagent.dom.render.cljs$core$IFn$_invoke$arity$2(new cljs.core.PersistentVector(null, 7, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"<>","<>",1280186386),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.counter.counter], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.temperature.temperature_converter], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.flight.flight_booker], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.timer.timer], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.crud.crud], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.circles.main], null)], null),document.getElementById("app"));
-});
-
-//# sourceMappingURL=app.core.js.map
