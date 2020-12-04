e47154faa14b43a1523dd9f9992ae31d6ebd820b
Parker Rueve <parkerrueve@Parkers-MacBook-Pro.local>
2020-12-04 13:05:57 -0500

    fix gitignore

--- resources/public/js.old/cljs-runtime/app.components.counter.js
+++
@@ -1,7 +0,0 @@
-goog.provide('app.components.counter');
-app.components.counter.counter = (function app$components$counter$counter(){
-var count = reagent.core.atom.cljs$core$IFn$_invoke$arity$1((0));
-return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),count], null);
-});
-
-//# sourceMappingURL=app.components.counter.js.map
