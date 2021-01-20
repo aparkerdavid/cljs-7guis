391a184cbf607726b83712aa61ddb9a621ed8fd6
Parker Rueve <parkerrueve@gmail.com>
2021-01-19 19:13:59 -0500

    refactor & test spreadsheet

--- src/main/app/spreadsheet.cljs
+++ src/main/app/spreadsheet.cljs
@@ -1,29 +1,25 @@
 (ns app.spreadsheet
   (:require [reagent.core :as r]
             [cljs.js :refer [eval js-eval]]
-            [cljs.reader :as reader]))
+            [cljs.reader :as reader]
+            [clojure.string :as string]))
+
 
 (def cell-letters-range
   (map js/String.fromCharCode (range (.charCodeAt "a") (inc (.charCodeAt "z")))))
 
+
 (def cell-numbers-range
   (range 1 100))
 
-(def cell-coordinate-keywords
-  (into #{}
-        (for [letter cell-letters-range
-              number cell-numbers-range]
-          (keyword (str letter number)))))
 
 (defonce
-  state
-  (r/atom
-   {:cells {}}))
+  state-1
+  (r/atom {}))
+
 
 (def eval-compiler-opts
-  "Bit of a hack here, I'll admit I don't fully understand what's going on.
-  As far as I can tell, the default behavior of cljs.js/eval is to treat unqualified symbols as being namespaced under cljs.user. This creates a problem when trying to call core functions, as eval is looking for them in the cljs.user namespace rather than cljs.core. The following options map is based on cljs.js/empty-state (apparently the default) but aliases the cljs.core namespace to cljs.user, allowing core functions to be used normally. It also allows use of symbols in the app.spreadsheet (i.e. current) namespace."
-
+  "Bit of a hack here. See implementation notes."
   (atom {:cljs.analyzer/namespaces {'cljs.user {:name 'cljs.core}
                                     'app.spreadsheet {:name 'app.spreadsheet}}
          :cljs.analyzer/constant-table {}
@@ -31,9 +27,10 @@
          :cljs.analyzer/externs nil
          :options {}}))
 
+
 (defn eval-list
   "Evaluate a list.
-  see: https://github.com/yogthos/cljs-eval-example/blob/master/src/cljs/cljs_eval_example/core.cljs "
+  see https://github.com/yogthos/cljs-eval-example/blob/master/src/cljs/cljs_eval_example/core.cljs"
   [list]
   (eval eval-compiler-opts
         list
@@ -42,34 +39,36 @@
          :context    :expr}
         :value))
 
+
 (defn root
   "Take the y-th root of x"
   [x y]
   (js/Math.pow x (/ 1 y)))
 
-(defn expand-formula
-  "Transform a Formula (defined as a list) into a list.
-  This list will later be evaluated as Clojure to determine the formula's value.
-  This function does not ensure that the Formula in question is valid Clojure (eval can handle that.)
-  This function does ensure that the Formula in question includes only supported operations.
-  It also replaces references to other Cells with the appropriate values.
+
+(defn expand-formula-1
+  "Prepare a Formula (defined as a list) for evaluation as Clojure.
+  This operation ensures that the Formula in question includes only supported operations.
+  It also replaces references to other Cells with their respective values.
+  This function does not ensure that the Formula in question is valid Clojure (eval handles that.)
   Each item in the list will be visited, and processed as such:
 
   If the item in question is a list, leave it alone
   (other than the empty list, which will become nil.)
   If it's a number, leave it alone.
-  If it is a cell, keyword, replace it with the value of the cell that it references.
-  If it is one of the supported arithmetic function symbols, replace it with the appropriate function
-  Anything else, replace it with nil.
-  "
-  [formula]
+  If it's a keyword, replace it with the value of the cell that it references.
+  If it's one of the supported function symbols, replace it with the appropriate function
+  Anything else: replace it with nil."
+  [formula state]
   (clojure.walk/postwalk
    (fn [item]
      (cond
        (= item '()) nil
        (list? item) item
        (number? item) item
-       (keyword? item) (-> @state :cells item :value)
+       (keyword? item)
+       ;; Keywords are converted to lowercase before lookup.
+       (-> item name string/lower-case keyword state :value)
        (= '+ item) 'cljs.core/+
        (= '- item) 'cljs.core/-
        (= '* item) 'cljs.core/*
@@ -80,18 +79,21 @@
        :else nil))
    formula))
 
-(defn solve-formula [formula-str]
-  ;; Check if formula-str should be treated as a number of a function by looking at its first char.
-  ;; If the first char is a number, Treat it as a number. Else, treat it as a function.
+
+(defn solve-formula-1
+  "Check if formula-str should be treated as a number of a function by looking at its first char.
+  If the first char is a number, Treat it as a number. Else, treat it as a function."
+  [formula-str state]
   (if (-> formula-str first js/parseInt js/isNaN)
     (->
     ;; If formula-str is a function, read it as if it were wrapped in parens.
      (reader/read-string (str \( formula-str \)))
-     (expand-formula)
+     (expand-formula-1 state)
      (eval-list))
 
     ;; If it is a number, read it as-is
     (reader/read-string formula-str)))
+
 
 (defn get-parents
   "A simple way to find a Cell's Parents: identify all the keywords in its Formula."
@@ -102,7 +104,8 @@
        flatten
        (filter keyword?)))
 
-(defn non-cyclical?
+
+(defn non-cyclical?-1
   "Check if a Formula would cause a cyclical reference if it were assigned to a given Cell, via BFS.
 
   First, find the Parents of the Formula in question.
@@ -110,90 +113,134 @@
   Otherwise: check if the Cell in question is among the Parents.
   If so: we've found a cyclical reference, return false.
   Otherwise: repeat the process with the combined set of the Parents of each Parent.
-  Eventually, we will either find a cyclical reference or run out of ancestors to test.
-  "
-  [state cell-id formula-str]
+  Eventually, we will either find a cyclical reference or run out of ancestors to test."
+  [cell-id formula-str state]
   (loop [current-batch (into #{} (get-parents formula-str))]
     (cond
       (current-batch cell-id) false
       (empty? current-batch) true
       :else (recur (->> current-batch
-                        (map #(r/cursor state [:cells % :formula]))
-                        (map deref)
+                        (map #(-> state % :formula))
                         (map get-parents)
                         flatten
                         (into #{}))))))
 
-(defn add-child!
+
+(defn add-child
   "Given two cells 'cell-id' and 'child-id', register child-id as a Child of cell-id."
-  [state cell-id child-id]
-  (swap! (r/cursor state [:cells cell-id :children])
-         #(into [] (conj % child-id))))
+  [state child-id cell-id]
+  (update-in state [cell-id :children] #(into [] (conj % child-id))))
 
-(defn remove-child!
+
+(defn remove-child
   "Given two Cells 'cell-id' and 'child-id', unregister child-id as a child of cell-id"
-  [state cell-id child-id]
-  (swap! (r/cursor state [:cells cell-id :children])
-         (fn [children] (filter #(not= child-id %) children))))
+  [state child-id cell-id]
+  (update-in state [cell-id :children]
+             (fn [children] (filter #(not= child-id %) children))))
 
-(defn update-value!
+
+(defn update-value
   "Set the Value of a given cell to the result of the evaluation of its Formula."
   [state cell-id]
-  (let [value (r/cursor state [:cells cell-id :value])
-        formula (r/cursor state [:cells cell-id :formula])]
-    (reset! value (solve-formula @formula))))
+  (assoc-in
+   state [cell-id :value]
+   (-> state cell-id :formula (solve-formula-1 state))))
 
-(defn update-value-chain!
+
+(comment
+  (-> {:a1 {:value 3 :formula "+ 1 3" :children [:b1]}
+       :b1 {:value 5 :formula "+ :a1 5"}}
+      ((comp)
+       #(update-value % :a1)
+       #(update-value % :b1))))
+
+(defn update-values
+  [state ids]
+  (let [update-fn (apply comp (for [id (reverse ids)] #(update-value % id)))]
+    (update-fn state)))
+
+
+(comment
+  (-> {:a1 {:value 6 :formula "+ 1 3" :children [:b1]}
+       :b1 {:value 1 :formula "+ :a1 5"}}
+      (update-value :a1)
+      (update-value :b1))
+  (-> {:a1 {:value 6 :formula "+ 1 3" :children [:b1]}
+       :b1 {:value 1 :formula "+ :a1 5"}}
+      ((comp #(update-value % :b1) #(update-value % :a1)))))
+
+
+(comment
+  (-> {:a1 {:value 6 :formula "+ 1 3" :children [:b1]}
+       :b1 {:value 1 :formula "+ :a1 5"}}
+      (update-values [:a1 :b1])))
+
+
+
+(defn update-value-chain
   "Update the values of a given cell and its descendents, in topological order via DFS."
   [state cell-id]
   (loop [to-visit [cell-id]
          visited #{}
          sorted []]
-    (let [current (peek to-visit)
-          unvisited-children (->> (r/cursor state [:cells current :children])
-                                  deref
-                                  (filter (complement visited))
-                                  not-empty)]
+    (let [current
+          (peek to-visit)
+          unvisited-children
+          (when current
+            (->> state
+                 current
+                 :children
+                 (filter (complement visited))
+                 not-empty))]
       (cond
         (empty? to-visit)
-        (doseq [id-to-update (reverse sorted)]
-          (update-value! state id-to-update))
+        (update-values state (reverse sorted))
 
         unvisited-children
-        (recur #_to-visit (into to-visit unvisited-children)
-               #_visited visited
-               #_sorted sorted)
+        (recur (into to-visit unvisited-children)
+               visited
+               sorted)
 
         :else
-        (recur #_to-visit (pop to-visit)
-               #_visited (conj visited current)
-               #_sorted (if (visited current) sorted (conj sorted current)))))))
+        (recur (pop to-visit)
+               (conj visited current)
+               (if (visited current) sorted (conj sorted current)))))))
 
-(defn update-formula!
-  "Change the formula associated with the a given cell, and 'do the needful' as they say.
+
+(defn update-formula
+  "Change the formula associated with the a given cell. Update it and its children.
 
   This is the core function of the spreadsheet's update mechanism."
-  [state cell-id formula-str]
-  (let [old-parents (-> (r/cursor state [:cells cell-id :formula]) deref get-parents)
-        new-parents (get-parents formula-str)]
-    (doseq [old-parent old-parents] (remove-child! state old-parent cell-id))
-    (doseq [new-parent new-parents] (add-child! state new-parent cell-id))
-    (reset! (r/cursor state [:cells cell-id :formula]) formula-str)
-    (update-value-chain! state cell-id)))
+  [cell-id formula-str state]
+  (let [old-parents
+        (-> state cell-id :formula get-parents)
+        remove-old-children
+        (apply comp
+               (for [parent old-parents]
+                 #(remove-child % cell-id parent)))
+        new-parents (get-parents formula-str)
+        add-new-children
+        (apply comp
+               (for [parent new-parents]
+                 #(add-child % cell-id parent)))]
+    (-> state
+        remove-old-children
+        add-new-children
+        (assoc-in [cell-id :formula] formula-str)
+        (update-value-chain cell-id))))
 
-(defn cell-field [state cell-id]
-  (let [editing (r/atom false)
-        cell-value (r/cursor state [:cells cell-id :value])
-        cell-formula (r/atom "")]
+
+(defn cell-field-1 [cell-id]
+  (let [editing-formula (r/atom nil)
+        cell-value (r/cursor state-1 [cell-id :value])]
     (fn []
       [:input
        {:type "text"
         :class ["w-24" "h-12"]
         :style {:position "relative"
                 :margin "-1px"}
-                ;; :z-index (if @editing "1" nil)
 
-        :value (if @editing @cell-formula @cell-value)
+        :value (or @editing-formula @cell-value)
 
         :on-key-press
         (fn [e]
@@ -201,20 +248,18 @@
             (.. js/document -activeElement blur)))
 
         :on-focus
-        (fn [e]
-          (reset! cell-formula (deref (r/cursor state [:cells cell-id :formula])))
-          (reset! editing true))
+        #(reset! editing-formula (deref (r/cursor state-1 [cell-id :formula])))
 
         :on-blur
-        (fn [e]
-          (when (non-cyclical? state cell-id @cell-formula)
-            (update-formula! state cell-id @cell-formula)
-            (reset! editing false)))
+        #(when (non-cyclical?-1 cell-id @editing-formula @state-1)
+           (swap! state-1 (partial update-formula cell-id @editing-formula))
+           (reset! editing-formula nil))
 
         :on-change
         (fn [e]
-          (when @editing
-            (reset! cell-formula (-> e .-target .-value))))}])))
+          (reset! editing-formula (-> e .-target .-value)))}])))
+
+
 
 (defn spreadsheet []
   [:div
@@ -247,11 +292,17 @@
          [:td
           {:class ["sticky" "left-0" "z-10"]}
           [:div
-           {:class ["align-middle" "border-2" "border-gray-600" "bg-gray-100" "p-2" "h-12" "w-24" "text-center"]
+           {:class ["align-middle"
+                    "border-2"
+                    "border-gray-600"
+                    "bg-gray-100" "p-2"
+                    "h-12"
+                    "w-24"
+                    "text-center"]
             :style {:margin "-2px"}}
            number]]
          (for [letter cell-letters-range]
            [:td
             {:key letter
              :class ["p-0"]}
-            [cell-field state  (keyword (str letter number))]])])]]]])
+            [cell-field-1 (keyword (str letter number))]])])]]]])
---
+++ src/test/spreadsheet_test.cljs
@@ -0,0 +1,98 @@
+(ns spreadsheet-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [app.spreadsheet :as spreadsheet]))
+
+(deftest non-cyclical?
+  (testing "A non-cyclical input"
+    (is (spreadsheet/non-cyclical?-1
+         {:cells {:a1 {:formula "+ :b1 :a2"} :b1 {:formula "+ :b2 :c3"}}}
+         :c3
+         "+ :h1 :b5")))
+  (testing "a cyclical input"
+    (is (not
+         (spreadsheet/non-cyclical?-1
+          :c3
+          "+ :a1 :b1"
+          {:a1 {:formula "+ :b1 :a2"} :b1 {:formula "+ :b2 :c3"}})))))
+
+(deftest add-child
+  (testing "Add a child"
+    (is (= (spreadsheet/add-child {} :b1 :a1)
+           {:a1 {:children [:b1]}}))))
+
+(deftest remove-child
+  (testing "Remove a child"
+    (is
+     (->
+      (spreadsheet/remove-child {:b1 {:children [:a1]}} :a1 :b1)
+      :b1
+      :children
+      empty?))))
+
+(deftest update-value
+  (testing "Update a cell's value"
+    (is (= (spreadsheet/update-value {:a1 {:value 4 :formula "+ 1 2"}} :a1)
+           {:a1 {:value 3, :formula "+ 1 2"}})))
+  (testing "Update a value with a reference"
+    (is (= 5
+           (->
+            (spreadsheet/update-value
+             {:a1 {:value 4 :formula "+ 2 2"}
+              :b1 {:value 4 :formula "+ 1 :a1"}}
+             :b1)
+            :b1 :value))))
+
+  (testing "Update multiple values in order"
+    (is (= {:a1 {:value 4, :formula "+ 1 3", :children [:b1]}
+            :b1 {:value 9, :formula "+ :a1 5"}}
+           (spreadsheet/update-values
+            {:a1 {:value 6 :formula "+ 1 3" :children [:b1]}
+             :b1 {:value 1 :formula "+ :a1 5"}}
+            [:a1 :b1])))))
+
+
+
+(deftest update-value-chain
+
+  (testing "Update a value chain"
+    (is (= {:a1 {:value 3 :formula "+ 1 2" :children [:b1]}
+            :b1 {:value 5 :formula "+ :a1 2"}}
+           (spreadsheet/update-value-chain
+            {:a1 {:value 4 :formula "+ 1 2" :children [:b1]}
+             :b1 {:value 2 :formula "+ :a1 2"}}
+            :a1))))
+
+  (testing "Update a formula with a child"
+    (is (= {:a1 {:value 4, :formula "+ 1 3", :children [:b1]}
+            :b1 {:value 9, :formula "+ :a1 5"}}
+           (spreadsheet/update-value-chain
+            {:a1 {:value 6 :formula "+ 1 3" :children [:b1]}
+             :b1 {:value 1 :formula "+ :a1 5"}}
+            :a1)))))
+
+(deftest update-formula
+
+  (testing "Add a formula"
+    (is (= (spreadsheet/update-formula
+            :a1 "2" {})
+           {:a1 {:value 2 :formula "2"}})))
+
+  (testing "Alter an existing formula"
+    (is (= (spreadsheet/update-formula
+            :a1 "4" {:a1 {:value 2 :formula "2"}})
+           {:a1 {:value 4 :formula "4"}})))
+
+  (testing "Add a formula with a dependency"
+    (is (= (spreadsheet/update-formula
+            :b1 "+ :a1 2" {:a1 {:value 2 :formula "2"}})
+           {:a1 {:value 2 :formula "2" :children [:b1]}
+            :b1 {:value 4 :formula "+ :a1 2"}})))
+
+  (testing "Update a formula with a child"
+    (is (= (spreadsheet/update-formula
+            :a1 "4"
+            {:a1 {:value 2 :formula "2" :children [:b1]}
+             :b1 {:value 4 :formula "+ :a1 2"}})
+           {:a1 {:value 4 :formula "4" :children [:b1]}
+            :b1 {:value 6 :formula "+ :a1 2"}}))))
+
