2f54aee1569aa4da14e3a2e2bb60522c190ac3a7
Parker Rueve <parkerrueve@gmail.com>
2021-01-20 21:51:52 -0500

    Minor cleanup in timer.cljs

--- src/main/app/timer.cljs
+++ src/main/app/timer.cljs
@@ -1,19 +1,11 @@
 (ns app.timer
   (:require [reagent.core :as r]))
 
-(defn limit-slider [value]
-  [:input
-   {:class ["w-full"]
-    :type "range"
-    :min "10"
-    :max "1000"
-    :value @value
-    :on-change (fn [e] (reset! value (-> e .-target .-value)))}])
 
 (defn timer []
   (let [active (r/atom false)
         elapsed (r/atom 0)
-        limit (r/atom 1000)
+        limit (r/atom 500)
         timer (. js/window setInterval
                  #(cond
                     (>= @elapsed @limit)
@@ -25,7 +17,7 @@
     (fn []
       [:div
        {:class ["max-w-xs"]}
-       [:input
+       [:input ;; A disabled input slider serves as our elapsed time readout.
         {:class ["w-full"]
          :type "range"
          :disabled true
@@ -33,9 +25,15 @@
          :max "1000"
          :value (* 1000 (/ @elapsed @limit))}]
        [:div
-        {:class "text-center"}
+        {:class ["text-center"]}
         (str @elapsed " / " @limit)]
-       [limit-slider limit]
+       [:input
+        {:class ["w-full"]
+         :type "range"
+         :min "10"
+         :max "1000"
+         :value @limit
+         :on-change (fn [e] (reset! limit (-> e .-target .-value)))}]
        [:div
         [:button
          {:class
@@ -48,11 +46,11 @@
               "font-bold"
               "border-red-600"]
              ["btn-green"]))
-          :on-click #(swap! active not)}
+          :on-click (fn [e] (swap! active not))}
          (if @active "Stop" "Start")]
         [:button
          {:class
           ["w-full"
            "btn-blue"]
-          :on-click  (reset! elapsed 0)}
+          :on-click (fn [e] (reset! elapsed 0))}
          "Reset"]]])))
