d7dc089aa6c94f8cd618b965515d81136067fd2f
Parker Rueve <parkerrueve@gmail.com>
2021-01-22 12:17:48 -0500

    Refactor timer

--- src/main/app/timer.cljs
+++ src/main/app/timer.cljs
@@ -1,56 +1,91 @@
 (ns app.timer
   (:require [reagent.core :as r]))
 
+(defonce state
+  (r/atom
+   {:interval nil
+    :elapsed 0
+    :limit 500}))
+
+
+(defn reset-timer!
+  []
+  (swap! state assoc :elapsed 0))
+
+
+(defn stop-timer!
+  []
+  (js/clearInterval (@state :interval))
+  (swap! state assoc :interval nil))
+
+
+(defn tick!
+  []
+  (if (>= (@state :elapsed) (@state :limit))
+    (do  (stop-timer!)
+         (reset-timer!))
+    (swap! state update :elapsed inc)))
+
+
+(defn start-timer!
+  []
+  (swap! state assoc :interval (js/setInterval tick! 20)))
+
+
+(defn seconds-str
+  "Convert a integer count of 20ms intervals to a string indicating the corresponding number of seconds.
+   Single-decimal point precision.
+   eg:
+   500 => 10.0s"
+  [n]
+  (str (. (* 0.02 n) toFixed 1) "s"))
+
 
 (defn timer []
-  (let [active (r/atom false)
-        elapsed (r/atom 0)
-        limit (r/atom 500)
-        timer (. js/window setInterval
-                 #(cond
-                    (>= @elapsed @limit)
-                    (do
-                      (reset! active false)
-                      (reset! elapsed 0))
-                    @active
-                    (swap! elapsed inc)))]
-    (fn []
-      [:div
-       {:class ["max-w-xs"]}
-       [:input ;; A disabled input slider serves as our elapsed time readout.
-        {:class ["w-full"]
-         :type "range"
-         :disabled true
-         :min "0"
-         :max "1000"
-         :value (* 1000 (/ @elapsed @limit))}]
-       [:div
-        {:class ["text-center"]}
-        (str @elapsed " / " @limit)]
-       [:input
-        {:class ["w-full"]
-         :type "range"
-         :min "10"
-         :max "1000"
-         :value @limit
-         :on-change (fn [e] (reset! limit (-> e .-target .-value)))}]
-       [:div
-        [:button
-         {:class
-          (concat
-           ["w-full"
-            "mb-4"]
-           (if @active
-             ["btn-red"
-              "text-red-600"
-              "font-bold"
-              "border-red-600"]
-             ["btn-green"]))
-          :on-click (fn [e] (swap! active not))}
-         (if @active "Stop" "Start")]
-        [:button
-         {:class
-          ["w-full"
-           "btn-blue"]
-          :on-click (fn [e] (reset! elapsed 0))}
-         "Reset"]]])))
+  [:div
+   {:class ["max-w-xs"]}
+   [:input ;; A disabled input slider serves as our elapsed time readout.
+    {:class ["w-full"]
+     :type "range"
+     :disabled true
+     :min "0"
+     :max "1000"
+     :value (* 1000 (/ (@state :elapsed) (@state :limit)))}]
+   [:div
+    {:class ["text-center"]}
+    (str (seconds-str (@state :elapsed)) " / " (seconds-str (@state :limit)))]
+   [:input
+    {:class ["w-full"]
+     :type "range"
+     :min "10"
+     :max "1000"
+     :value (@state :limit)
+     :on-change
+     (fn [e] (swap! state assoc :limit (-> e .-target .-value)))}]
+   [:div
+
+    (if (@state :interval)
+      [:button
+       {:class
+        ["w-full"
+         "mb-4"
+         "btn-red"
+         "text-red-600"
+         "font-bold"
+         "border-red-600"]
+        :on-click stop-timer!}
+       "Stop"]
+      [:button
+       {:class
+        ["w-full"
+         "mb-4"
+         "btn-green"]
+        :on-click start-timer!}
+       "Start"])
+
+    [:button
+     {:class
+      ["w-full"
+       "btn-blue"]
+      :on-click reset-timer!}
+     "Reset"]]])
