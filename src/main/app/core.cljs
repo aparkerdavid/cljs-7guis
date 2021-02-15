cb372c9afd8a6079df17ce42d31f8991abc81fcf
Parker Rueve <parkerrueve@gmail.com>
2021-02-15 01:14:25 -0500

    No need for a separate namespace just for layout.

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -12,6 +12,15 @@
    [reagent.dom :as rd]))
 
 
+(defn block
+  [title desc content]
+  [:div {:class ["mt-16"]}
+   [:div {:class ["max-w-prose" "mx-auto" "overflow-visible"]}
+    [:h2  title]
+    [:div {:class ["mt-2"]} desc]
+    [:div {:class ["mt-12"]} content]]])
+
+
 
 (defn counter-block
   []
