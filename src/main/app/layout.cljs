2b973aacb1df018baadeb8247ee9367194008a9e
Parker Rueve <parkerrueve@gmail.com>
2021-02-15 01:14:47 -0500

    No need for a separate namespace just for layout.

--- src/main/app/layout.cljs
+++
@@ -1,9 +0,0 @@
-(ns app.layout)
-
-(defn block
-  [title desc content]
-  [:div {:class ["mt-16"]}
-   [:div {:class ["max-w-prose" "mx-auto" "overflow-visible"]}
-    [:h2  title]
-    [:div {:class ["mt-2"]} desc]
-    [:div {:class ["mt-12"]} content]]])
